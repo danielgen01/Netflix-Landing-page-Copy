ba43211debb26bb6859783129322d68fdc433a76
Daniel <gendik38@gmail.com>
2023-02-04 13:48:20 +0100

    adding normal netflix font

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,10 +4,15 @@ import './Main.css'
 const Main = () => {
   return (
     <div className='Main'>
+      <div className='background'>
       <img src='https://assets.nflxext.com/ffe/siteui/vlv3/862cc171-8df5-418c-886f-2aaf767ae159/a2446503-ce3b-45a5-85f8-5dbe8c5dde72/DE-de-20230130-popsignuptwoweeks-perspective_alpha_website_large.jpg'
       id='background-cover' />
+      </div>
       <img src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' 
       id='logo'/>
+      <div className='top-buttons'>
+        
+        </div>
     </div>
   )
 }
