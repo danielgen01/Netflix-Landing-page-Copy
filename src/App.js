d5067468fd0ba3906a8daf172b1720d72da1e7ce
Daniel <gendik38@gmail.com>
2023-02-06 15:53:58 +0100

    add Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 // import each component here
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
